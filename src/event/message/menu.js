926c5a967f663c0cf2adbb7c84fe508737c33469
j0rtiz <j0rtiz@protonmail.ch>
2025-03-17 18:16:38 -0300

    Remoção de lib não usada

--- src/event/message/menu.js
+++ src/event/message/menu.js
@@ -1,9 +1,7 @@
 const { menu, submenu } = require("../../../config/config.json");
 
 const items = Object.keys(menu.items).map((item) => menu.items[item].step);
-const subitems = Object.keys(submenu.items).map(
-  (item) => submenu.items[item].step
-);
+const subitems = Object.keys(submenu.items).map((item) => submenu.items[item].step);
 const mainMenu = [menu.title, menu.subtitle, ...items].join("\n");
 const otherMenu = [menu.title, menu.otherSubtitle, ...items].join("\n");
 const subMenu = [submenu.title, ...subitems].join("\n");
