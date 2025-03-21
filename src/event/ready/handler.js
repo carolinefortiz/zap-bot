baf656433a33a287d91291531db1e22225c863d3
j0rtiz <j0rtiz@protonmail.ch>
2025-03-20 22:15:43 -0300

    Monitoria

--- src/event/ready/handler.js
+++ src/event/ready/handler.js
@@ -1,7 +1,9 @@
 const { existsSync, unlinkSync } = require("node:fs");
+const { watch } = require("../../core/monitor");
 
 const handler = async () => {
   if (existsSync("./qr-code.png")) unlinkSync("./qr-code.png");
+  setInterval(watch, 10000);
   console.log("O Bot iniciou com sucesso!");
 };
 
